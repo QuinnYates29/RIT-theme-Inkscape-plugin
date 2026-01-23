1015ed3a7eb3cbe5c8ba1ffeca98465edaf863ea
QuinnYates29 <quinnyates3229@gmail.com>
2026-01-23 00:28:44 -0500

    Added arguments for extension 1, created TreeNode structure for parsing boxes inside of boxes

--- my_square_extension.py
+++ my_square_extension.py
@@ -20,17 +20,80 @@
 """
 Description of this extension
 """
+from idlelib.tree import TreeNode
 
 import inkex
+from inkex import Rectangle
+from lxml import etree
 
-class SquareExtension(inkex.EffectExtension):
-    """Please rename this class, don't keep it unnamed"""
+# COLORS
+BACKGROUND_COLOR = "#dcdcdc"
+LIGHT_GREEN = "#b6d7a8"
+DARK_BLUE = "#69a4d9"
+LIGHT_BLUE = "#cfe2f3"
+PURPLE = "#b665a8"
+RIT_ORANGE = "#F76902"
+RIT_BLACK  = "#000000"
+RIT_GRAY   = "#808080"
+RIT_WHITE  = "#FFFFFF"
+
+class TreeNode:
+    name: str
+    children: list[TreeNode]
+
+
+class ParentBox(inkex.EffectExtension):
+    def box_style(self, fill):
+        return {
+            "fill": fill,
+            "stroke": RIT_BLACK,
+            "stroke-width": str(self.svg.unittouu("1mm"))
+        }
+
     def add_arguments(self, pars):
-        pars.add_argument("Size", type=inkex.int,\
-            help="Picks the size of the square")
+        pars.add_argument("--title", type=str, help="Title for the top of the box", default="Schematic")
+        pars.add_argument("--height", type=str, help="Height of the box", default="500mm")
+        pars.add_argument("--width", type=str, help="Width of the box", default="500mm")
+        pars.add_argument("--str_data", type=str, default="")
+
+    def generate_tree(self, data, head):
+
+
+    def draw_box(self, x, y, width, height, fill = BACKGROUND_COLOR):
+        rect = Rectangle(
+            x=str(x),
+            y=str(y),
+            width=str(width),
+            height=str(height),
+            rx=str(self.svg.unittouu("3mm")),
+            ry=str(self.svg.unittouu("3mm"))
+        )
+        rect.style = self.box_style(fill)
+        self.svg.get_current_layer().add(rect)
+
+    def text(self, x, y, content):
+        t = inkex.TextElement(
+            x=str(x),
+            y=str(y)
+        )
+        t.style = {
+            "font-size": "12px",
+            "text-anchor": "middle",
+            "dominant-baseline": "middle"
+        }
+        t.text = content
+        self.svg.get_current_layer().add(t)
 
     def effect(self):
-        self.msg("This is an empty extension, please see tutorials for more details.")
+        layer = self.svg.get_current_layer()
+
+        x = 100
+        y = 100
+        width = self.svg.unittouu(self.options.width)
+        height = self.svg.unittouu(self.options.height)
+
+        self.draw_box(x, y, width, height)
+        self.text(x + width/3, y + width/3, self.options.top_text)
 
 if __name__ == '__main__':
-    SquareExtension().run()
+    Box().run()
